0d79be4efbae211e3961fd86575fb539fb88e49f
rajdev08 <groovy.srk@gmail.com>
2025-09-22 13:13:11 -0400

    update base

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
